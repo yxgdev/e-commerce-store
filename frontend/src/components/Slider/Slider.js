380683087a91225def034ea8eaca0aff968039c9
yxgdev <yeexuan73@gmail.com>
2022-02-01 22:56:14 +0800

    added some components

--- frontend/src/components/Slider/Slider.js
+++ frontend/src/components/Slider/Slider.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { Container, Arrow } from "./SliderStyles";
+import {
+  Container,
+  Arrow,
+  Wrapper,
+  InfoContainer,
+  Image,
+  ImgContainer,
+} from "./SliderStyles";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeftIcon />
       </Arrow>
-      <Arrow>
+      <Wrapper>
+        <ImgContainer>
+          <Image />
+        </ImgContainer>
+        <InfoContainer></InfoContainer>
+      </Wrapper>
+      <Arrow direction="right">
         <ArrowRightIcon />
       </Arrow>
     </Container>
--- frontend/src/components/Slider/SliderStyles.js
+++ frontend/src/components/Slider/SliderStyles.js
@@ -16,4 +16,29 @@ export const Arrow = styled.div`
   align-items: center;
   justify-content: center;
   position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
 `;
+
+export const Wrapper = styled.div`
+  height: 100%;
+`;
+
+export const Slide = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const ImgContainer = styled.div`
+  flex: 1;
+`;
+export const InfoContainer = styled.div`
+  flex: 1;
+`;
+
+export const Image = styled.img``;
