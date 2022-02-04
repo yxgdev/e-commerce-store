7a2910abef2adc2cbc659225f722cfdb60e5f983
yxgdev <yeexuan73@gmail.com>
2022-02-05 02:44:02 +0800

    added full slider

--- frontend/src/components/Slider/Slider.js
+++ frontend/src/components/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Arrow,
@@ -6,22 +6,44 @@ import {
   InfoContainer,
   Image,
   ImgContainer,
+  Slide,
+  Button,
+  Title,
+  Desc,
 } from "./SliderStyles";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { sliderItems } from "../../data";
+
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftIcon />
       </Arrow>
-      <Wrapper>
-        <ImgContainer>
-          <Image />
-        </ImgContainer>
-        <InfoContainer></InfoContainer>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightIcon />
       </Arrow>
     </Container>
--- frontend/src/components/Slider/SliderStyles.js
+++ frontend/src/components/Slider/SliderStyles.js
@@ -5,6 +5,7 @@ export const Container = styled.div`
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 export const Arrow = styled.div`
@@ -24,21 +25,48 @@ export const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 export const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 export const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
+  background-color: #${(props) => props.bg};
 `;
 export const ImgContainer = styled.div`
+  height: 100%;
   flex: 1;
 `;
 export const InfoContainer = styled.div`
   flex: 1;
+  padding: 50px;
 `;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  height: 80%;
+`;
+
+export const Title = styled.h1`
+  font-size: 70px;
+`;
+export const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 2px;
+`;
+export const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
