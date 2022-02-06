bcf4f0b4c60745ee72fd4e7cfae1c250c6c9fab0
yxgdev <yeexuan73@gmail.com>
2022-02-06 23:35:42 +0800

    finished user routes

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const authHeader = req.header.token;
-
+  const authHeader = req.headers.token;
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) res.status(403).json("token is not valid!");
       req.user = user;
@@ -17,6 +17,16 @@ export const verifyToken = (req, res, next) => {
 export const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("not allowed to do thiss");
+    }
+  });
+};
+
+export const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
       next();
     } else {
       res.status(403).json("not allowed to do thiss");
